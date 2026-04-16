45540b627eb7407969cf72ff344d334580de3c9c
hana <hanasellami18@gmail.com>
2026-04-16 14:10:32 +0100

    first commit

---
+++ src/app/admin/admin-topbar/admin-topbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-topbar',
+  standalone: false,
+  templateUrl: './admin-topbar.html',
+  styleUrl: './admin-topbar.css',
+})
+export class AdminTopbar {
+
+}
